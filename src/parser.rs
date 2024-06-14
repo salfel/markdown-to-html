538a8448d965e08a9624a7ab756e810f1105eee1
Felix Salcher <dev.felix.salcher@gmail.com>
2024-06-14 20:40:33 +0200

    added ability for parser to parse bold and underline

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,5 @@
-use std::borrow::BorrowMut;
-
 use crate::lexer::Token;
+use std::borrow::BorrowMut;
 
 pub struct Parser {
     lines: Vec<Vec<Token>>,
@@ -22,15 +21,17 @@ impl Parser {
                     Token::Heading1 => {
                         Statement::Heading1(Self::parse_expression(iterator.borrow_mut(), None))
                     }
-                    Token::Asterisk(_) => {
-                        Statement::Plain(Self::parse_expression(iterator.borrow_mut(), None))
-                    }
-                    Token::Underscore(_) => {
-                        Statement::Plain(Self::parse_expression(iterator.borrow_mut(), None))
-                    }
+                    Token::Asterisk(count) => Statement::Plain(Self::parse_expression(
+                        iterator.borrow_mut(),
+                        Some(Token::Asterisk(count)),
+                    )),
+                    Token::Underscore(count) => Statement::Plain(Self::parse_expression(
+                        iterator.borrow_mut(),
+                        Some(Token::Underscore(count)),
+                    )),
                     Token::Word(content) => Statement::Plain(Self::parse_expression(
                         iterator.borrow_mut(),
-                        Some(content),
+                        Some(Token::Word(content)),
                     )),
                 };
 
@@ -43,35 +44,72 @@ impl Parser {
 
     fn parse_expression(
         iterator: &mut dyn Iterator<Item = Token>,
-        current: Option<String>,
+        current: Option<Token>,
     ) -> Expression {
-        let mut tokens = vec![];
-        if let Some(word) = current {
-            tokens.push(Token::Word(word));
-        }
+        match current {
+            Some(Token::Asterisk(_)) => {
+                let mut tokens = vec![];
 
-        for value in iterator {
-            tokens.push(value);
-        }
-
-        let token = tokens
-            .into_iter()
-            .map(|token| {
-                if let Token::Word(content) = token {
-                    content
-                } else {
-                    String::from("")
+                for token in &mut *iterator {
+                    match token {
+                        Token::Asterisk(_) => {
+                            break;
+                        }
+                        _ => {
+                            tokens.push(token);
+                        }
+                    }
                 }
-            })
-            .collect::<Vec<String>>()
-            .join(" ");
 
-        Expression::Plain(token)
+                let mut iterator = tokens.into_iter();
+                let current = iterator.next();
+                Expression::Bold(Box::new(Self::parse_expression(
+                    iterator.borrow_mut(),
+                    current,
+                )))
+            }
+            Some(Token::Underscore(_)) => {
+                let mut tokens = vec![];
+
+                for token in &mut *iterator {
+                    match token {
+                        Token::Underscore(_) => {
+                            break;
+                        }
+                        _ => {
+                            tokens.push(token);
+                        }
+                    }
+                }
+
+                let mut iterator = tokens.into_iter();
+                let current = iterator.next();
+                Expression::Underline(Box::new(Self::parse_expression(
+                    iterator.borrow_mut(),
+                    current,
+                )))
+            }
+            Some(Token::Word(content)) => {
+                let mut tokens = vec![content];
+                while let Some(Token::Word(content)) = iterator.next() {
+                    tokens.push(content);
+                }
+
+                Expression::Plain(tokens.join(" "))
+            }
+            None => {
+                let current = iterator.next();
+                Self::parse_expression(iterator, current)
+            }
+            _ => Expression::Plain(String::from("")),
+        }
     }
 }
 
 #[derive(Debug, PartialEq)]
 pub enum Expression {
+    Underline(Box<Expression>),
+    Bold(Box<Expression>),
     Plain(String),
 }
 
@@ -88,7 +126,10 @@ mod tests {
 
     #[test]
     fn parses_tokens() {
-        let lexer = Lexer::new(String::from("# something\nsomething else"));
+        let lexer = Lexer::new(String::from(
+            "# **something**
+__**something else**__",
+        ));
         let tokens = lexer.get_tokens();
 
         let parser = Parser::new(tokens);
@@ -97,8 +138,12 @@ mod tests {
         assert_eq!(
             statements,
             vec![
-                Statement::Heading1(Expression::Plain(String::from("something"))),
-                Statement::Plain(Expression::Plain(String::from("something else")))
+                Statement::Heading1(Expression::Bold(Box::new(Expression::Plain(String::from(
+                    "something"
+                ))))),
+                Statement::Plain(Expression::Underline(Box::new(Expression::Bold(Box::new(
+                    Expression::Plain(String::from("something else"))
+                )))))
             ]
         );
     }
