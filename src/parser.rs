815db36affbfc4585f33e236961d3cba18efb3b7
Felix Salcher <dev.felix.salcher@gmail.com>
2024-06-14 22:41:11 +0200

    added additional checks for bold

--- src/parser.rs
+++ src/parser.rs
@@ -47,18 +47,43 @@ impl Parser {
         current: Option<Token>,
     ) -> Expression {
         match current {
-            Some(Token::Asterisk(_)) => {
+            Some(Token::Asterisk(left_count)) => {
                 let mut tokens = vec![];
+                let mut right_count = 0;
 
-                for token in &mut *iterator {
+                for token in iterator {
                     match token {
-                        Token::Asterisk(_) => {
+                        Token::Asterisk(count) => {
+                            right_count = count;
                             break;
                         }
                         _ => {
                             tokens.push(token);
                         }
                     }
+                }
+
+                if right_count <= 1 {
+                    Self::prepend_string(&mut tokens, String::from("*"), left_count as usize);
+                    Self::append_string(&mut tokens, String::from("*"), right_count as usize);
+                    let mut iterator = tokens.into_iter();
+                    let current = iterator.next();
+                    return Self::parse_expression(iterator.borrow_mut(), current);
+                } else if left_count >= 3 {
+                    Self::prepend_string(&mut tokens, String::from("*"), (left_count - 2) as usize);
+                } else if left_count == 1 {
+                    Self::prepend_string(&mut tokens, String::from("*"), 1);
+                    Self::append_string(&mut tokens, String::from("*"), right_count as usize);
+
+                    let mut iterator = tokens.into_iter();
+                    let current = iterator.next();
+                    return Self::parse_expression(iterator.borrow_mut(), current);
+                }
+
+                if right_count >= 3 {
+                    Self::append_string(&mut tokens, String::from("*"), (right_count - 2) as usize);
+                } else if right_count == 1 {
+                    Self::append_string(&mut tokens, String::from("*"), 1);
                 }
 
                 let mut iterator = tokens.into_iter();
@@ -71,7 +96,7 @@ impl Parser {
             Some(Token::Underscore(_)) => {
                 let mut tokens = vec![];
 
-                for token in &mut *iterator {
+                for token in iterator {
                     match token {
                         Token::Underscore(_) => {
                             break;
@@ -95,13 +120,33 @@ impl Parser {
                     tokens.push(content);
                 }
 
-                Expression::Plain(tokens.join(" "))
+                Expression::Text(tokens.join(" "))
             }
             None => {
                 let current = iterator.next();
                 Self::parse_expression(iterator, current)
             }
-            _ => Expression::Plain(String::from("")),
+            _ => Expression::Text(String::from("")),
+        }
+    }
+    fn append_string(tokens: &mut Vec<Token>, string: String, count: usize) {
+        if let Some(last_token) = tokens.last_mut() {
+            if let Token::Word(content) = last_token {
+                *last_token = Token::Word(content.to_string() + &string.repeat(count));
+            } else {
+                tokens.push(Token::Word(string.repeat(count)));
+            }
+        }
+    }
+
+    fn prepend_string(tokens: &mut [Token], string: String, count: usize) {
+        if let Some(first_token) = tokens.first_mut() {
+            if let Token::Word(content) = first_token {
+                *first_token = Token::Word(string.repeat(count) + content);
+            } else {
+                tokens.rotate_right(1);
+                tokens[0] = Token::Word(string.repeat(count));
+            }
         }
     }
 }
@@ -110,7 +155,7 @@ impl Parser {
 pub enum Expression {
     Underline(Box<Expression>),
     Bold(Box<Expression>),
-    Plain(String),
+    Text(String),
 }
 
 #[derive(Debug, PartialEq)]
@@ -124,27 +169,63 @@ mod tests {
     use super::*;
     use crate::lexer::Lexer;
 
-    #[test]
-    fn parses_tokens() {
-        let lexer = Lexer::new(String::from(
-            "# **something**
-__**something else**__",
-        ));
+    fn setup_parser(input: String) -> Vec<Statement> {
+        let lexer = Lexer::new(input);
         let tokens = lexer.get_tokens();
 
         let parser = Parser::new(tokens);
-        let statements = parser.get_statements();
+        parser.get_statements()
+    }
+
+    #[test]
+    fn parses_tokens() {
+        let statements = setup_parser(String::from(
+            "# ***something**
+# **something***
+__**something else**__",
+        ));
 
         assert_eq!(
             statements,
             vec![
-                Statement::Heading1(Expression::Bold(Box::new(Expression::Plain(String::from(
-                    "something"
+                Statement::Heading1(Expression::Bold(Box::new(Expression::Text(String::from(
+                    "*something"
+                ))))),
+                Statement::Heading1(Expression::Bold(Box::new(Expression::Text(String::from(
+                    "something*"
                 ))))),
                 Statement::Plain(Expression::Underline(Box::new(Expression::Bold(Box::new(
-                    Expression::Plain(String::from("something else"))
+                    Expression::Text(String::from("something else"))
                 )))))
             ]
         );
+    }
+
+    #[test]
+    fn check_overflow() {
+        let statements = setup_parser(String::from(
+            "**something*
+***something**
+*something**
+**something***
+***something***",
+        ));
+
+        assert_eq!(
+            statements,
+            vec![
+                Statement::Plain(Expression::Text(String::from("**something*"))),
+                Statement::Plain(Expression::Bold(Box::new(Expression::Text(String::from(
+                    "*something"
+                ))))),
+                Statement::Plain(Expression::Text(String::from("*something**"))),
+                Statement::Plain(Expression::Bold(Box::new(Expression::Text(String::from(
+                    "something*"
+                ))))),
+                Statement::Plain(Expression::Bold(Box::new(Expression::Text(String::from(
+                    "*something*"
+                ))))),
+            ]
+        )
     }
 }
