4668a601500513d57b3f21ae8e1d89d52d2885cb
Felix Salcher <dev.felix.salcher@gmail.com>
2024-06-15 12:33:16 +0200

    added evaluation to evaluator

--- src/evaluater.rs
+++ src/evaluater.rs
@@ -1,4 +1,4 @@
-use crate::parser::{Parser, Statement};
+use crate::parser::{Expression, Parser, Statement};
 
 pub struct Evaluator {
     statements: Vec<Statement>,
@@ -13,7 +13,40 @@ impl Evaluator {
     }
 
     pub fn eval(self) -> String {
-        String::new()
+        let mut body = String::new();
+
+        for statement in self.statements {
+            let content = match statement {
+                Statement::Heading1(expression) => {
+                    format!("<h1>{}</h1>", Self::eval_expression(expression))
+                }
+                Statement::Plain(expression) => {
+                    format!("<p>{}</p>", Self::eval_expression(expression))
+                }
+            };
+
+            body += &content;
+        }
+
+        body
+    }
+
+    pub fn eval_expression(expression: Expression) -> String {
+        match expression {
+            Expression::Text(content) => content,
+            Expression::Bold(expression) => {
+                format!("<strong>{}</strong>", Self::eval_expression(*expression))
+            }
+            Expression::Italic(expression) => {
+                format!("<i>{}</i>", Self::eval_expression(*expression))
+            }
+            Expression::BoldItalic(expression) => {
+                format!(
+                    "<strong><i>{}</i></strong>",
+                    Self::eval_expression(*expression)
+                )
+            }
+        }
     }
 }
 
@@ -23,9 +56,18 @@ mod tests {
 
     #[test]
     fn evaluates_heading() {
-        let evaluator = Evaluator::new(String::from("# heading"));
+        let evaluator = Evaluator::new(String::from(
+            "# heading
+plain
+# **heading**
+# *heading*
+***paragraph***",
+        ));
         let result = evaluator.eval();
 
-        assert_eq!(result, "<h1>heading<h1>")
+        assert_eq!(
+            result,
+            "<h1>heading</h1><p>plain</p><h1><strong>heading</strong></h1><h1><i>heading</i></h1><p><strong><i>paragraph</i></strong></p>"
+        )
     }
 }
