8e70f855d35312d7c98b1d8f2894339ff27563f4
Felix Salcher <felix.salcher@additive.eu>
2024-06-21 14:16:53 +0200

    removed code from v1

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,8 @@
-use evaluater::Evaluator;
-
 use std::{fs, process};
-
-pub mod evaluater;
-pub mod lexer;
-pub mod parser;
 
 fn main() {
     let filepath = "./input.md";
     let contents = get_contents(filepath);
-
-    let evaluater = Evaluator::new(contents);
-    let evaluated = evaluater.eval();
-
-    write_to_file("./index.html", evaluated)
-}
-
-fn write_to_file(filepath: &str, contents: String) {
-    match fs::write(filepath, contents) {
-        Ok(()) => {}
-        Err(message) => {
-            println!("Err, message: {}", message);
-            process::exit(1);
-        }
-    }
 }
 
 fn get_contents(filepath: &str) -> String {
